b814f8979eba809753f328475f673ecce7334e3d
Ay Has <ahashim983@yahoo.com>
2019-03-21 21:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-function takeANumber(katzDeliLine, name){
-  return `Welcome ${name}. You are number ${katzDeliLine} in line.`
-}
-
 function nowServing(katzDeliLine){
-  if katzDeliLine != []{
-    return katzDeliLine[0]
-    katzDeliLine.unshift()
-  } else {
+  if (katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+    katzDeliLine.slice(1)
+  } else if (katzDeliLine.length == 0){
     return "There is nobody waiting to be served!"
   }
 }
 
 function currentLine(katzDeliLine){
-  if katzDeliLine == []{
+  if (katzDeliLine.length == 0){
     return "The line is currently empty."
   } else {
-    var queue = 0
-     for(var i = 0; i < katzDeliLine.length; i++){
-       queue = queue + 1
+    var queueDict = []
+    var i = 0
+    for(i = 0; i < katzDeliLine.length; i++){
+       queueDict.push(`${i+1}. ${katzDeliLine[i]}`)
      }
-     return Object.assign({},{[queue] : katzDeliLine})
-
+     return queueDict.replace(\[,"")
   }
-
-  }
+}
