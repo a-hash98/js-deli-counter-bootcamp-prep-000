3d75aba4a342cefe9298531bffc087f73dd52dd6
Ay Has <ahashim983@yahoo.com>
2019-03-21 21:41:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine.shift()}.`
-    katzDeliLine.slice(1)
-  } else if (katzDeliLine.length == 0){
-    return "There is nobody waiting to be served!"
-  }
+function takeANumber(katzDeliLine, name){
+  var position = katzDeliLine.indexOf(name)+1
+  return `Welcome ${name}. You are number ${position} in line.`
 }
 
-function currentLine(katzDeliLine){
-  if (katzDeliLine.length == 0){
-    return "The line is currently empty."
-  } else {
-    var queueDict = []
-    var i = 0
-    for(i = 0; i < katzDeliLine.length; i++){
-       queueDict.push(`${i+1}. ${katzDeliLine[i]}`)
-     }
-     return queueDict.replace(\[,"")
-  }
-}
