309e4949da683590155257d4f3a1f5edd1bd2664
Ay Has <ahashim983@yahoo.com>
2019-03-21 21:46:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(katzDeliLine, name){
   var position = parseInt(katzDeliLine.indexOf(name))
-  return position
   return `Welcome, ${name}. You are number ${position+1} in line.`
 }
