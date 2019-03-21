8d2663632fec9892714373d88ba77c3776fe798e
Ay Has <ahashim983@yahoo.com>
2019-03-21 21:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   return `Currently serving ${katzDeliLine[0]}.`
-  katzDeliLine.unshift()
+  katzDeliLine.slice(1)
 }
